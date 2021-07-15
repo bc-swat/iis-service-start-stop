8c0185531c2179a1d967ad1e1d0a91d66423b1b3
Roy Wong <roy.wong2@extendhealth.com>
2021-07-15 16:25:50 -0600

    Start and Stop Script changes

--- stop.ps1
+++ stop.ps1
@@ -1,41 +1,23 @@
+param(
+    [parameter(Mandatory = $true)]
+    [string]$server,
+    [parameter(Mandatory = $true)]
+    [string]$app_pool_name,
+    [parameter(Mandatory = $true)]
+    [string]$user_id,
+    [parameter(Mandatory = $true)]
+    [SecureString]$password
+)
+
 Write-Host "Stop IIS"
+Write-Output "Server: $server - App Pool: $app_pool_name"
 
-# # Get AppPool Name
-# $appPoolName = $OctopusParameters['appPoolName']
-# # Get the number of retries
-# $retries = $OctopusParameters['appPoolCheckRetries']
-# # Get the number of attempts
-# $delay = $OctopusParameters['appPoolCheckDelay']
+$credential = [PSCredential]::new($user_id, $password)
 
-# # Check if exists
-# if (Test-Path IIS:\AppPools\$appPoolName) {
-
-#     # Stop App Pool if not already stopped
-#     if ((Get-WebAppPoolState $appPoolName).Value -ne "Stopped") {
-#         Write-Output "Stopping IIS app pool $appPoolName"
-#         Stop-WebAppPool $appPoolName
-
-#         $state = (Get-WebAppPoolState $appPoolName).Value
-#         $counter = 1
-
-#         # Wait for the app pool to the "Stopped" before proceeding
-#         do {
-#             $state = (Get-WebAppPoolState $appPoolName).Value
-#             Write-Output "$counter/$retries Waiting for IIS app pool $appPoolName to shut down completely. Current status: $state"
-#             $counter++
-#             Start-Sleep -Milliseconds $delay
-#         }
-#         while ($state -ne "Stopped" -and $counter -le $retries)
-
-#         # Throw an error if the app pool is not stopped
-#         if ($counter -gt $retries) {
-#             throw "Could not shut down IIS app pool $appPoolName. `nTry to increase the number of retries ($retries) or delay between attempts ($delay milliseconds)."
-#         }
-#     }
-#     else {
-#         Write-Output "$appPoolName already Stopped"
-#     }
-# }
-# else {
-#     Write-Output "IIS app pool $appPoolName doesn't exist"
-# }
+Write-Output "Stopping IIS app pool $appPoolName"
+Invoke-Command -ComputerName $server `
+    -Credential $credential `
+    -ScriptBlock { `
+        Import-Module WebAdministration; `
+        Stop-WebAppPool -Name $app_pool_name `
+}
