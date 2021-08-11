32e95bee88b60c3dc346fc9be1d646aed4b203bd
Roy Wong <roy.wong2@extendhealth.com>
2021-08-11 11:16:28 -0600

    Minus cert

--- site-action.ps1
+++ site-action.ps1
@@ -10,34 +10,32 @@ function site_create {
 
     # Get the key data
     # [Byte[]]$cert_data = Get-Content -Path $web_site_cert_path -Encoding Byte
-    $cert_file_parts = $web_site_cert_path.Replace('/', '\').Split('\')
-    $cert_file_name = $cert_file_parts[$cert_file_parts.Length - 1]
-    $cert_file_path = (Join-Path -Path $web_site_path -ChildPath $cert_file_name)
+    # $cert_file_parts = $web_site_cert_path.Replace('/', '\').Split('\')
+    # $cert_file_name = $cert_file_parts[$cert_file_parts.Length - 1]
+    # $cert_file_path = (Join-Path -Path $web_site_path -ChildPath $cert_file_name)
 
     return {
-        Write-Output "Cert File Path: $cert_file_path"
+        # create app pool if it doesn't exist
+        if (Get-IISAppPool -Name $Using:app_pool_name) {
+            Write-Output "The App Pool $Using:app_pool_name already exists"
+        }
+        else {
+            Write-Output "Creating app pool $Using:app_pool_name"
+            $app_pool = New-WebAppPool -Name $Using:app_pool_name
+            $app_pool.autoStart = $true
+            $app_pool.managedPipelineMode = "Integrated"
+            $app_pool | Set-Item
+            Write-Output "App pool $Using:app_pool_name has been created"
+        }
 
-        # # create app pool if it doesn't exist
-        # if (Get-IISAppPool -Name $Using:app_pool_name) {
-        #     Write-Output "The App Pool $Using:app_pool_name already exists"
-        # }
-        # else {
-        #     Write-Output "Creating app pool $Using:app_pool_name"
-        #     $app_pool = New-WebAppPool -Name $Using:app_pool_name
-        #     $app_pool.autoStart = $true
-        #     $app_pool.managedPipelineMode = "Integrated"
-        #     $app_pool | Set-Item
-        #     Write-Output "App pool $Using:app_pool_name has been created"
-        # }
-
-        # # create the folder if it doesn't exist
-        # if (Test-path $Using:web_site_path) {
-        #     Write-Output "The folder $Using:web_site_path already exists"
-        # }
-        # else {
-        #     New-Item -ItemType Directory -Path $Using:web_site_path -Force
-        #     Write-Output "Created folder $Using:web_site_path"
-        # }
+        # create the folder if it doesn't exist
+        if (Test-path $Using:web_site_path) {
+            Write-Output "The folder $Using:web_site_path already exists"
+        }
+        else {
+            New-Item -ItemType Directory -Path $Using:web_site_path -Force
+            Write-Output "Created folder $Using:web_site_path"
+        }
 
         # #write out the cert
         # $cert_store_path = Cert:\LocalMachine\Root\
@@ -60,20 +58,20 @@ function site_create {
         #         -FriendlyName
         # }
 
-        # # create the site if it doesn't exist
-        # $iis_site = Get-IISSite -Name $Using:web_site_name
-        # if ($iis_site) {
-        #     Write-Output "The site $Using:web_site_name already exists"
-        # }
-        # else {
-        #     Write-Output "Creating IIS site $Using:web_site_name"
-        #     $iis_site = New-WebSite -Name $Using:web_site_name `
-        #         -HostHeader $Using:web_site_host_header `
-        #         -Ssl -Port 443 `
-        #         -SslFlags 0 `
-        #         -PhysicalPath $Using:web_site_path `
-        #         -ApplicationPool $Using:app_pool_name
-        # }
+        # create the site if it doesn't exist
+        $iis_site = Get-IISSite -Name $Using:web_site_name
+        if ($iis_site) {
+            Write-Output "The site $Using:web_site_name already exists"
+        }
+        else {
+            Write-Output "Creating IIS site $Using:web_site_name"
+            $iis_site = New-WebSite -Name $Using:web_site_name `
+                -HostHeader $Using:web_site_host_header `
+                -Ssl -Port 443 `
+                -SslFlags 0 `
+                -PhysicalPath $Using:web_site_path `
+                -ApplicationPool $Using:app_pool_name
+        }
 
         # $binding = Get-WebBinding -Name $Using:web_site_name -Protocol "https"
         # if (!$binding) {
